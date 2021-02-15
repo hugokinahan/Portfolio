5176a228847a7f4647de80255107177c20d146e3
Hugo Kinahan <hugokinahan@hotmail.com>
2021-02-15 16:32:17 +0000

    first draft finished

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,29 +1,21 @@
 import React from 'react'
-import Sky from 'react-sky'
 // import { Icon } from 'semantic-ui-react'
 import { GrMail, GrLink } from 'react-icons/gr'
+import { BsChevronCompactDown } from 'react-icons/bs'
 // import { Link } from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
+import ReactRotatingText from 'react-rotating-text'
 
 import project1 from '../src/images/finished.png'
 import project2 from '../src/images/large-home.png'
 import project3 from '../src/images/homepage.png'
 import project4 from '../src/images/sharebnb-homepage.png'
 import mac from '../src/images/mac.png'
-
+import profileImage from '../src/images/profile-pic.JPG'
 
 function App() {
   return (
     <main className='page-layout'>
-      <Sky
-        images={{
-          0: 'https://media-exp1.licdn.com/dms/image/C4D03AQEmgtdJa6jOOQ/profile-displayphoto-shrink_200_200/0/1587581731827?e=1618444800&v=beta&t=tyic_pnk0K5p1n8LHXvket-8Un2rIXFdXxsciZ9Vnvs',
-        }}
-        how={1}
-        time={30} 
-        size={'200px'} 
-        background={'palettedvioletred'} 
-      />
       <div className='contact-icon'>
         <div className='icon-link'>
           <a href='https://www.linkedin.com/in/hugo-kinahan/' >
@@ -61,22 +53,20 @@ function App() {
         </div>
       </div>
       <div className='title'>
-        <h1>Hugo Kinahan</h1>
+        {/* <h1 className='typewriter'>Hugo Kinahan</h1> */}
+        <ReactRotatingText className='title-name' items={['HUGO KINAHAN']} pause={100000} cursor={false} typingInterval={100}/>
+        <i className="chevron-icon"><BsChevronCompactDown /></i>
       </div>
-      {/* <section className="header-image">
-        <h1>
-          <div className="extra-gap">Hugo</div>
-          <div>
-            <span>Kinahan</span>
-          </div>
-        </h1>
-      </section> */}
-      <h2 id='about-me'>About Me</h2>
+      <h1 id='about-me'>About Me</h1>
       <div className='bio'>
-        {/* <img className='profile-pic' src='https://media-exp1.licdn.com/dms/image/C4D03AQEmgtdJa6jOOQ/profile-displayphoto-shrink_200_200/0/1587581731827?e=1618444800&v=beta&t=tyic_pnk0K5p1n8LHXvket-8Un2rIXFdXxsciZ9Vnvs'/> */}
-        <p className='bio-text'>Hi, I&apos;m Hugo, a Software Engineer based in London. </p>
+        <img className='profile-pic' src={profileImage}/>
+        <div className='bio-text'>
+          <h4>Hi, I&apos;m Hugo, a Software Engineer based in London. </h4>
+          <p>I have always had a passion for tech and have always dreamed of turning an idea into something tangible such as a website. During the Covid pandemic I decided to take this passion a step further, so I enrolled on the Software Engineering Immersive Course at General Assembly. Over an amazing 12 weeks I completed four projects in a variety of languages and I was assured that this is the career for me.</p>
+          <p>I am now looking to join an enthusiastic and ambitious team with an exciting product and a large scope for me to develop my skills.</p>
+        </div>
       </div>
-      <h2 id='languages'>Languages & Frameworks</h2>
+      <h1 id='languages'>Languages & Frameworks</h1>
       <div className='languages'>
         <div className='icon-layout'>
           <i className="devicon-react-original plain"></i>
@@ -176,7 +166,7 @@ function App() {
         </div>
       </div>
       <div id='my-work' className='my-work-section'>
-        <h2>My Work</h2>
+        <h1>My Work</h1>
         <h3>General Assembly</h3>
         <img src='https://www.drupal.org/files/general-assembly_logo.png' />
         <div className='projects'>
@@ -293,23 +283,43 @@ function App() {
           </div>
         </div>
       </div>
-      <h2 id='contact'>Get In Touch</h2>
+      <h1 id='contact'>Get In Touch</h1>
       <div className='footer'>
         <div className='footer-icons'>
-          <a href='https://www.linkedin.com/in/hugo-kinahan/' >
-            <i className="devicon-linkedin-plain"></i>
-          </a>
-          <a href='https://twitter.com/BestOfKin_' >
-            <i className="devicon-twitter-original"></i>
-          </a>
-          <a href='https://github.com/hugokinahan' >
-            <i className="devicon-github-original"></i>
-          </a>
-          <a href='hugokinahan@hotmail.com' >
-            <i className="index-icon"><GrMail /></i>
-          </a>
+          <div className='social-icons'>
+            <a href='https://www.linkedin.com/in/hugo-kinahan/' >
+              <i className="devicon-linkedin-plain"></i>
+            </a>
+            <a href='https://twitter.com/BestOfKin_' >
+              <i className="devicon-twitter-original"></i>
+            </a>
+          </div>
+          <div className='footer-links'>
+            <a href='https://www.linkedin.com/in/hugo-kinahan/' >
+              <p>linkedin.com/in/hugo-kinahan/</p>
+            </a>
+            <a href='https://twitter.com/BestOfKin_' > 
+              <p>@BestofKin_</p>
+            </a>
+          </div>
+          <div className='social-icons'>
+            <a href='https://github.com/hugokinahan' >
+              <i className="devicon-github-original"></i>
+            </a>
+            <a href='hugokinahan@hotmail.com' >
+              <i className="index-icon"><GrMail /></i>
+            </a>
+          </div>
+          <div className='footer-links'>
+            <a href='https://github.com/hugokinahan' >
+              <p>github.com/hugokinahan</p>
+            </a>
+            <a href='hugokinahan@hotmail.com' >
+              <p>hugokinahan@hotmail.com</p>
+            </a>
+          </div>
         </div>
-        <div className='footer-links'>
+        {/* <div className='footer-links'>
           <a href='https://www.linkedin.com/in/hugo-kinahan/' >
             <p>linkedin.com/in/hugo-kinahan/</p>
           </a>
@@ -322,7 +332,7 @@ function App() {
           <a href='hugokinahan@hotmail.com' >
             <p>hugokinahan@hotmail.com</p>
           </a>
-        </div>
+        </div> */}
       </div>
     </main>
   )
