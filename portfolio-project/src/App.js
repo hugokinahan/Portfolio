6d18261f923de8f4c73a44ca7eaee3bcc364c2a5
Hugo Kinahan <hugokinahan@hotmail.com>
2021-02-15 13:22:33 +0000

    lots of changes. Macs, buttons, big title. In a good place

--- portfolio-project/src/App.js
+++ portfolio-project/src/App.js
@@ -1,42 +1,63 @@
 import React from 'react'
-// import Sky from 'react-sky'
+import Sky from 'react-sky'
 // import { Icon } from 'semantic-ui-react'
-import { GrMail } from 'react-icons/gr'
+import { GrMail, GrLink } from 'react-icons/gr'
+// import { Link } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
 import project1 from '../src/images/finished.png'
 import project2 from '../src/images/large-home.png'
 import project3 from '../src/images/homepage.png'
 import project4 from '../src/images/sharebnb-homepage.png'
+import mac from '../src/images/mac.png'
 
 
 function App() {
   return (
     <main className='page-layout'>
-      {/* <Sky
+      <Sky
         images={{
           0: 'https://media-exp1.licdn.com/dms/image/C4D03AQEmgtdJa6jOOQ/profile-displayphoto-shrink_200_200/0/1587581731827?e=1618444800&v=beta&t=tyic_pnk0K5p1n8LHXvket-8Un2rIXFdXxsciZ9Vnvs',
         }}
         how={1}
         time={30} 
-        size={'150px'} 
+        size={'200px'} 
         background={'palettedvioletred'} 
-      /> */}
+      />
       <div className='contact-icon'>
         <div className='icon-link'>
-          <i className="devicon-linkedin-plain"></i>
-          <p>linkedin.com/in/hugo-kinahan/</p>
+          <a href='https://www.linkedin.com/in/hugo-kinahan/' >
+            <i className="devicon-linkedin-plain"></i>
+          </a>
         </div>
         <div className='icon-link'>
-          <i className="devicon-twitter-original"></i>
-          <p>@BestofKin_</p>
+          <a href='https://twitter.com/BestOfKin_' >
+            <i className="devicon-twitter-original"></i>
+          </a>
         </div>
         <div className='icon-link'>
-          <i className="devicon-github-original"></i>
-          <p>github.com/hugokinahan</p>
+          <a href='https://github.com/hugokinahan' >
+            <i className="devicon-github-original"></i>
+          </a>
         </div>
         <div className='icon-link'>
-          <i className="index-icon"><GrMail /></i>
-          <p>hugokinahan@hotmail.com</p>
+          <a href='hugokinahan@hotmail.com' >
+            <i className="index-icon"><GrMail /></i>
+          </a>
+        </div>
+        <div className='nav'>
+          <a href='#about-me'>
+            <p>about.</p>
+          </a>
+          <a href='#languages'>
+            <p>languages.</p>
+          </a>
+          <a href='#my-work'>
+            <p>my work.</p>
+          </a>
+          <a href='#contact'>
+            <p>contact.</p>
+          </a>
         </div>
       </div>
       <div className='title'>
@@ -50,12 +71,12 @@ function App() {
           </div>
         </h1>
       </section> */}
-      <h2>About Me</h2>
+      <h2 id='about-me'>About Me</h2>
       <div className='bio'>
-        <img className='profile-pic' src='https://media-exp1.licdn.com/dms/image/C4D03AQEmgtdJa6jOOQ/profile-displayphoto-shrink_200_200/0/1587581731827?e=1618444800&v=beta&t=tyic_pnk0K5p1n8LHXvket-8Un2rIXFdXxsciZ9Vnvs'/>
-        <p className='bio-text'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc condimentum lobortis suscipit. Etiam enim elit, venenatis eu magna ac, imperdiet dignissim nisl. Vestibulum congue dictum ipsum. Fusce eu interdum libero, eget sollicitudin arcu. Suspendisse sed arcu rhoncus, tempor leo in, commodo nunc. Maecenas dictum nisi eget feugiat viverra. Curabitur mollis augue sit amet nisi elementum, vitae facilisis arcu faucibus. Donec aliquet diam mi, at vulputate turpis condimentum ut. Nunc est magna, sodales ut semper eu, laoreet nec elit.</p>
+        {/* <img className='profile-pic' src='https://media-exp1.licdn.com/dms/image/C4D03AQEmgtdJa6jOOQ/profile-displayphoto-shrink_200_200/0/1587581731827?e=1618444800&v=beta&t=tyic_pnk0K5p1n8LHXvket-8Un2rIXFdXxsciZ9Vnvs'/> */}
+        <p className='bio-text'>Hi, I&apos;m Hugo, a Software Engineer based in London. </p>
       </div>
-      <h2>Languages & Frameworks</h2>
+      <h2 id='languages'>Languages & Frameworks</h2>
       <div className='languages'>
         <div className='icon-layout'>
           <i className="devicon-react-original plain"></i>
@@ -154,14 +175,25 @@ function App() {
           <p>Yarn</p>
         </div>
       </div>
-      <div className='my-work-section'>
+      <div id='my-work' className='my-work-section'>
         <h2>My Work</h2>
         <h3>General Assembly</h3>
         <img src='https://www.drupal.org/files/general-assembly_logo.png' />
         <div className='projects'>
           <div className='project-border'>
             <h4>#4 Sharebnb</h4>
-            <img src={project4} />
+            <a href='https://share-bnb.herokuapp.com/' >
+              <img className='project-image' src={project4} />
+              <img src={mac} />
+            </a>
+            <div className='project-buttons' >
+              <a href='https://github.com/hugokinahan/sei-project-4' >
+                <Button className='link-button'><p><i className="devicon-github-original" /> Link To GitHub</p></Button>
+              </a>
+              <a href='https://share-bnb.herokuapp.com/' >
+                <Button className='link-button'><p><i ><GrLink /> Link To Website</i></p></Button>
+              </a>
+            </div>
             <div className='icon-layout'>
               <i className="devicon-html5-plain plain"></i>
               <i className="devicon-css3-plain"></i>
@@ -180,7 +212,18 @@ function App() {
           </div>
           <div className='project-border'>
             <h4>#3 2020 Reimagined</h4>
-            <img src={project3} />
+            <a href='https://project-3-2020-reimagined.herokuapp.com/' >
+              <img className='project-image' src={project3} />
+              <img src={mac} />
+            </a>
+            <div className='project-buttons' >
+              <a href='https://github.com/hugokinahan/sei-project-3' >
+                <Button className='link-button'><p><i className="devicon-github-original" /> Link To GitHub</p></Button>
+              </a>
+              <a href='https://project-3-2020-reimagined.herokuapp.com/' >
+                <Button className='link-button'><p><i ><GrLink /> Link To Website</i></p></Button>
+              </a>
+            </div>
             <div className='icon-layout'>
               <i className="devicon-html5-plain plain"></i>
               <i className="devicon-css3-plain"></i>
@@ -199,7 +242,18 @@ function App() {
           </div>
           <div className='project-border'>
             <h4>#2 Superhero Showdown</h4>
-            <img src={project2} />
+            <a href='https://superhero-showdown-hugokinahan.netlify.app/' >
+              <img className='project-image' src={project2} />
+              <img src={mac} />
+            </a>
+            <div className='project-buttons' >
+              <a href='https://github.com/hugokinahan/sei-project-2' >
+                <Button className='link-button'><p><i className="devicon-github-original" /> Link To GitHub</p></Button>
+              </a>
+              <a href='https://superhero-showdown-hugokinahan.netlify.app/' >
+                <Button className='link-button'><p><i ><GrLink /> Link To Website</i></p></Button>
+              </a>
+            </div>
             <div className='icon-layout'>
               <i className="devicon-html5-plain plain"></i>
               <i className="devicon-css3-plain"></i>
@@ -216,7 +270,18 @@ function App() {
           </div>
           <div className='project-border'>
             <h4>#1 The Wizard Of Oz</h4>
-            <img src={project1} />
+            <a href='https://hugokinahan.github.io/sei-project-1/' >
+              <img className='project-image' src={project1} />
+              <img src={mac} />
+            </a>
+            <div className='project-buttons' >
+              <a href='https://github.com/hugokinahan/sei-project-1' >
+                <Button className='link-button'><p><i className="devicon-github-original" /> Link To GitHub</p></Button>
+              </a>
+              <a href='https://hugokinahan.github.io/sei-project-1/' >
+                <Button className='link-button'><p><i ><GrLink /> Link To Website</i></p></Button>
+              </a>
+            </div>
             <div className='icon-layout'>
               <i className="devicon-html5-plain plain"></i>
               <i className="devicon-css3-plain"></i>
@@ -226,6 +291,37 @@ function App() {
             </div>
             <p>Follow The Yellow Brick Road. A grid-based game using HTML, CSS and JavaScript based on the classic arcade game &apos;Frogger&apos;. Time-frame: 1 week.</p>
           </div>
+        </div>
+      </div>
+      <h2 id='contact'>Get In Touch</h2>
+      <div className='footer'>
+        <div className='footer-icons'>
+          <a href='https://www.linkedin.com/in/hugo-kinahan/' >
+            <i className="devicon-linkedin-plain"></i>
+          </a>
+          <a href='https://twitter.com/BestOfKin_' >
+            <i className="devicon-twitter-original"></i>
+          </a>
+          <a href='https://github.com/hugokinahan' >
+            <i className="devicon-github-original"></i>
+          </a>
+          <a href='hugokinahan@hotmail.com' >
+            <i className="index-icon"><GrMail /></i>
+          </a>
+        </div>
+        <div className='footer-links'>
+          <a href='https://www.linkedin.com/in/hugo-kinahan/' >
+            <p>linkedin.com/in/hugo-kinahan/</p>
+          </a>
+          <a href='https://twitter.com/BestOfKin_' > 
+            <p>@BestofKin_</p>
+          </a>
+          <a href='https://github.com/hugokinahan' >
+            <p>github.com/hugokinahan</p>
+          </a>
+          <a href='hugokinahan@hotmail.com' >
+            <p>hugokinahan@hotmail.com</p>
+          </a>
         </div>
       </div>
     </main>
