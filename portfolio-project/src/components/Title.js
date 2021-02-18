ccc9c688caa1555268c16449f48512c59e85805b
Hugo Kinahan <hugokinahan@hotmail.com>
2021-02-18 10:59:40 +0000

    react sky added to home page

--- portfolio-project/src/components/Title.js
+++ portfolio-project/src/components/Title.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import Sky from 'react-sky'
 import ReactRotatingText from 'react-rotating-text'
 import { BsChevronCompactDown } from 'react-icons/bs'
+
+import ReactIcon from '../images/react-icon.png'
 
 function Title() {
 
   return (
     <div className='title'>
+      <Sky
+        images={{
+          /* FORMAT AS FOLLOWS */
+          0: ReactIcon,  /* You can pass as many images as you want */
+        }}
+        how={50} /* Pass the number of images Sky will render chosing randomly */
+        time={40} /* time of animation */
+        size={'100px'} /* size of the rendered images */
+        background={'palettedvioletred'} /* color of background */
+      />
       {/* <h1 className='typewriter'>Hugo Kinahan</h1> */}
       <ReactRotatingText className='title-name' items={['HUGO KINAHAN']} pause={100000} cursor={false} typingInterval={100}/>
       <div className='sub-heading'>
