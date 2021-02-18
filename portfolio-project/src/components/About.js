0055ecd1b3ef0813203fb41530f03c493e852099
Hugo Kinahan <hugokinahan@hotmail.com>
2021-02-18 09:56:48 +0000

    mobile optimised for iphone x portrait and blurb changed

--- portfolio-project/src/components/About.js
+++ portfolio-project/src/components/About.js
@@ -11,8 +11,8 @@ function About() {
         <img className='profile-pic' src={profileImage}/>
         <div className='bio-text'>
           <h4>Hi, I&apos;m Hugo, a Software Engineer based in London. </h4>
-          <p>I have always had a passion for tech and have always dreamed of turning an idea into something tangible such as a website. During the Covid pandemic I decided to take this passion a step further, so I enrolled on the Software Engineering Immersive Course at General Assembly. Over an amazing 12 weeks I completed four projects in a variety of languages and I was assured that this is the career for me.</p>
-          <p>I am now looking to join an enthusiastic and ambitious team with an exciting product and a large scope for me to develop my skills.</p>
+          <p>Since I was young, I have always had a passion for technology. Now, a few years on, I dream of turning inspirational and creative ideas into something tangible such as a website. During the Covid pandemic I decided to take my fervour a step further, so I enrolled on the Software Engineering Immersive Course at General Assembly. Over an amazingly informative and edifying 12 weeks I completed four projects in a variety of languages and I was assured, this is the career for me.</p>
+          <p>I am now looking to join an enthusiastic and ambitious team with an exciting product and large scope for me to develop my skills.</p>
         </div>
       </div>
     </div>
