036a85b7c452728a866959c0a39081206ed86d1f
Hugo Kinahan <hugokinahan@hotmail.com>
2021-02-18 16:13:44 +0000

    few tiny changes

--- portfolio-project/src/components/Title.js
+++ portfolio-project/src/components/Title.js
@@ -4,6 +4,10 @@ import ReactRotatingText from 'react-rotating-text'
 import { BsChevronCompactDown } from 'react-icons/bs'
 
 import ReactIcon from '../images/react-icon.png'
+// import JSIcon from '../images/JavaScript.png'
+// import HTMLIcon from '../images/html5.png'
+// // import CSSIcon from '../images/css.jpg'
+// import NodeIcon from '../images/node.jpg'
 
 function Title() {
 
@@ -12,11 +16,14 @@ function Title() {
       <Sky
         images={{
           /* FORMAT AS FOLLOWS */
-          0: ReactIcon,  /* You can pass as many images as you want */
+          0: ReactIcon, 
+          // 1: JSIcon, /* You can pass as many images as you want */
+          // 2: HTMLIcon,
+          // 3: NodeIcon,
         }}
         how={50} /* Pass the number of images Sky will render chosing randomly */
         time={40} /* time of animation */
-        size={'100px'} /* size of the rendered images */
+        size={'50px'} /* size of the rendered images */
         background={'palettedvioletred'} /* color of background */
       />
       {/* <h1 className='typewriter'>Hugo Kinahan</h1> */}
